8290b6b70453bcdbe3d5adec24b000efd5d2083b
Ionel Condor <icondor@sdl.com>
2016-03-26 13:06:49 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Salut Lume!");
+        System.out.println("Salut Lume 2!");
+        System.out.println("Salut Lume 3!");
+    }
+}
